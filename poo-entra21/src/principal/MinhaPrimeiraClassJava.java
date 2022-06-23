e41ae469c18c1d350852fb60c6b1a879a2daebba
guest01 <guest01@FLE207D21.alunos.sc.senac.br>
2022-06-22 21:20:41 -0300

    Aula do dia 22/06/2022

---
+++ poo-entra21/src/principal/MinhaPrimeiraClassJava.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class MinhaPrimeiraClassJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String texto = "turma";
+		System.out.println("Olá!" + texto);
+
+	}
+
+}
